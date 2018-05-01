7d69506a4df1c96c3cf35f3cc774f85f1e33d013
Brandon Moser <brandon.moser@digi.com>
2018-05-01 16:48:01 -0500

    Update copyright and description on LR54 reboot

--- LR54/lr54_reboot.py
+++ LR54/lr54_reboot.py
@@ -1,9 +1,23 @@
+#############################################################################
+# Copyright 2018, Digi International Inc.                                   #
+#                                                                           #
+# This Source Code Form is subject to the terms of the Mozilla Public       #
+# License, v. 2.0. If a copy of the MPL was not distributed with this       #
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.                  #
+#                                                                           #
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  #
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF          #
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR   #
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES    #
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN     #
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF   #
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.            #
+#############################################################################
+
 # ============================================
 # LR54 Quick Reboot Script
 # ============================================
-# Update REBOOT_TIME (in seconds) to set the
-# amount of time to wait before rebooting
-# the router
+# See #usage on changing default options
 # ============================================
 
 import sys
